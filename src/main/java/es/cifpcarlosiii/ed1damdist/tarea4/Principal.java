23ef3c808822af2bbd343d06bc56dd65f9722a06
AlejandroBenedictoMorga <5943051@alu.murciaeduca.es>
2026-06-15 22:45:11 +0200

    Aplicada refactorización al código: ocultación de atributos, constructores con nombre y método toString

--- src/main/java/es/cifpcarlosiii/ed1damdist/tarea4/Principal.java
+++ src/main/java/es/cifpcarlosiii/ed1damdist/tarea4/Principal.java
@@ -1,4 +1,11 @@
-package paqAnimales;
+package es.cifpcarlosiii.ed1damdist.tarea4;
+
+/**
+ * Clase principal que ejecuta la aplicación y gestiona las instancias del reino animal.
+ * @author Alejandro Benedicto Morga
+ * @version 1.0
+ */
+
 
 public class Principal {
 
@@ -16,25 +23,25 @@ Esquema de clases de reino-animal
 */
 
 
-        Animal ani = new Animal();
+        Animal animal = new Animal("Animal1");
 
-        Mamifero mami = new Mamifero();
+        Mamifero mamifero = new Mamifero("Mamifero1");
 
-        Perro toby = new Perro();
+        Perro toby = new Perro("Toby");
 
-        Gato isidoro = new Gato();
-        isidoro.pelos = 4;
+        Gato isidoro = new Gato("Isidoro");
+        isidoro.setPelos(4);
 
-        ani = isidoro;
+        animal = isidoro;
 
         Gato g;
-        g = (Gato) ani;
-        System.out.println("pelos de gato: " + g.pelos);
+        g = (Gato) animal;
+        System.out.println("pelos de gato: " + g.getPelos());
 
         Animal array[] = new Animal[4];
 
-        array[0] = ani;
-        array[1] = mami;
+        array[0] = animal;
+        array[1] = mamifero;
         array[2] = toby;
         array[3] = isidoro;
 
